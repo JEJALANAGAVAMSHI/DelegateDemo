8ec282772a10832c1d79a660fcc346fd69468273
JEJALA NAGAVAMSHI <nagavamshi8211@gmail.com>
2024-06-07 20:07:45 +0530

    Add project files.

---
+++ DelegateDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace DelegateDemo
+{
+    internal class Program
+    {
+        delegate void GreetingDelegate(string message);
+        static void Main(string[] args)
+        {
+            GreetingDelegate greetingDelegate = new GreetingDelegate(Greet);
+            greetingDelegate("Delegate is ok");
+        }
+        static void Greet(string info)
+        {
+            Console.WriteLine(info);
+        }
+
+        static void Print()
+        {
+
+        }
+
+        class Demo
+        {
+            GreetingDelegate greeting = new GreetingDelegate(Greet);
+            greeting("Hi");
+        }
+    }
+}
